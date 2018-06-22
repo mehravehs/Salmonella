0bd1140495e7fbe5f9323680c1090d696cf10c4e
Mehraveh <Mehraveh@AdministorsAir2.wireless.yale.internal>
2018-06-22 10:32:26 -0400

    final

--- Codes/Salmonella.m
+++ Codes/Salmonella.m
@@ -1,6 +1,7 @@
 clc
 clear all
 close all
+addpath(genpath('/Users/Mehraveh/Documents/MATLAB/'))
 load /Users/Mehraveh/Documents/MATLAB/Reza/probability_all_9.mat
 figure
 
@@ -48,4 +49,185 @@ correctY=correctY'
 mean(index==correctY)
 for i=1:9
     acc(i)=mean(correctY(correctY==i)==index(correctY==i))
+end
+
+%%
+addpath(genpath('/Users/Mehraveh/Documents/MATLAB/'))
+% clear all; close all; clc
+Salmonella_data = csvread('/Users/Mehraveh/Documents/MATLAB/Reza/Salmonella/Species101017_clean_PTMs.csv');
+[~,j]=ismember(Salmonella_data(:,1),[8,9])
+Salmonella_data(j==0,:)=[];
+addpath(genpath('/Users/Mehraveh/Documents/MATLAB/Parcellation/2017'))
+% l = 174
+l = size(Salmonella_data,1)
+V = Salmonella_data(1:l,2:end)';
+% load label_but_col.mat
+load imp_feature.mat
+load Acc.mat
+
+
+[sorted_imp, idx] = sort(imp,'descend')
+% label = label(idx,:)
+A = V(idx(1:2),:)
+[coeff,score,latent,tsquared,explained] = pca(A');
+score=score';
+%%
+% recall = false negative, precision = false positive
+close all
+figure
+cmap = othercolor('Spectral4',length(precision))
+for i = 1:size(V,2)    
+    s=scatter(A(1,i),A(2,i),500,'MarkerFaceColor',cmap(Salmonella_data(i,1)-min(Salmonella_data(:,1))+1,:),'MarkerEdgeColor','none');, hold on          
+    alpha(s,0.8)
+%     legendInfo{i} = ['X = ' num2str(i)];
+end
+
+clear h
+for i = 1:length(precision)
+    h(i) = plot(NaN,NaN,'.','color',cmap(i,:),'MarkerSize',50), hold on  
+end
+% legend('1','2')
+samples = {'S. cerevesiae','K. aerogenes', 'HEK', 'HeLa', 'S typhimurium',...
+    'E coli', 'H Salinarium','Contaminated Milk', 'Uncontaminated Milk', ...
+    'Uncontaminated Milk-Butros', 'Listeria monocytogenes Spiked Milk 10^5-Butros',...
+    'Ecoli Spiked Milk 10^4-Butros', 'Klebsiella aerogenes Spiked Milk 10^5-Butros',...
+    'Ecoli Spiked Milk 10^5-Butros'}
+
+samples={'Uncontaminated Milk-Butros', 'Listeria monocytogenes Spiked Milk 10^5-Butros',...
+    'Ecoli Spiked Milk 10^4-Butros', 'Klebsiella aerogenes Spiked Milk 10^5-Butros',...
+    'Ecoli Spiked Milk 10^5-Butros'}
+
+
+samples={'Contaminated Milk','Uncontaminated Milk'}
+legend(h,samples');
+
+% legend(legendInfo)
+set(gca,'box','off')
+axis('square')
+set(gca,'Ticklength', [0 0])
+set(gca,'FontSize',25); 
+% set(gca,'xticklabelrotation',45); 
+set(gca,'FontWeight','normal');
+color = get(gca,'Color');
+grid on
+
+xlabel(sprintf('First important PTM (%s)',label(1,find(~isspace(label(1,:))))));
+ylabel(sprintf('Second important PTM (%s)',label(2,find(~isspace(label(2,:))))));
+xlim([0.035,0.08])
+ylim([0.65,0.875])
+% yruler = ax1.YRuler;
+% yruler.Axle.Visible = 'off';
+% xruler = ax1.XRuler;
+% xruler.Axle.Visible = 'off';
+set(gca,'color','none')
+
+% [~, I] = unique(C); %
+% I = length(C) - I(length(I):-1:1); 
+% p = findobj(gca,'Type','Patch');
+% legend(p(I),'X','Y','Z');
+
+%%
+figure
+accs = [precision;recall]'
+cmap = othercolor('Spectral4',length(precision))
+% b = bar(accs,'LineWidth',1.5,'FaceColor',[0 .5 .5],'EdgeColor',[0 .9 .9],'LineWidth',1.5)
+c = [[0,0,0];[0.5,0.5,0.5]]
+x1 = -0.15:1:length(precision)
+x2 = 0.15:1:length(precision)+0.25
+for i = 1:size(accs,1)
+    b = bar(x1(i),accs(i,1),0.25,'FaceColor',cmap(i,:),'EdgeColor','none','LineWidth',1.5),hold on 
+    b = bar(x2(i),accs(i,2),0.25,'FaceColor',cmap(i,:),'EdgeColor','none','LineWidth',1.5),hold on 
+    alpha(b,0.5)
+end
+
+
+clear h
+h(1) = bar(NaN,NaN,'FaceColor',[0,0,0]);
+h(2) = bar(NaN,NaN,'FaceColor',[0.5,0.5,0.5]);
+legend(h,{'Precision';'Recall'});
+axis square
+set(gca,'XTick',0:1:length(precision)-1)
+set(gca,'XTickLabelRotation',0)
+set(gca,'XTickLabel',samples)
+
+set(gca,'box','off')
+set(gca,'Ticklength', [0 0])
+set(gca,'FontSize',30); 
+set(gca,'FontWeight','normal');
+color = get(gca,'Color');
+grid on
+
+% ylim([0.5,1.05])
+ylabel('Identification Accuracy') 
+
+%%
+load label.mat
+load imp_feature.mat
+load Acc.mat
+[sorted_imp, idx] = sort(imp,'descend')
+label = label(idx,:)
+for i=1:length(label)    
+    label_cell{i} = label(i,find(~isspace(label(i,:))));
+end
+figure
+cmap = (jet)
+barh(sorted_imp)
+for i = 1:size(sorted_imp,2)
+    b = bar(i,sorted_imp(i),0.75,'FaceColor',cmap(i,:),'EdgeColor','none','LineWidth',1.5),hold on         
+end
+
+
+set(gca,'XTick',1:size(sorted_imp,2))
+set(gca,'XTickLabelRotation',45)
+set(gca,'XTickLabel',label_cell)
+set(gca,'box','off')
+set(gca,'Ticklength', [0 0])
+set(gca,'fontsize',12) 
+set(gca,'FontWeight','normal');
+color = get(gca,'Color');
+grid on
+ylabel('Feature Importance','fontsize',25) 
+xlabel('Features (PTMs)','fontsize',25)
+
+
+
+
+%% exemplar
+
+V(isnan(V)) = 0;
+t = size(V,1);
+n = size(V,2);
+mean_subtract = mean(V,2);
+V = V - repmat(mean_subtract,[1,n]);   
+twoNorm = sqrt(sum(abs(V).^2,1));
+% m = max(twoNorm);
+% V = V/m;
+V = V./twoNorm;
+
+sqDistances_HCP{1} = sqDistance(V);
+
+
+imagesc(1-sqDistances_HCP{1});
+
+D = sqDistances_HCP{1};
+e0 = zeros(t,1);
+e0(1)=e0(1)+3;
+d0 = sqDistance_Y(V,e0);
+
+if min(d0)<=max(max(D(1:n,1:n)))
+    fprintf('No :( \n')
+end       
+d0_HCP{1} = d0;
+K = max(Salmonella_data(1:l,1));
+S_opt = exemplar(sqDistances_HCP,d0_HCP,t,n,K,1);
+S_opt = sort(S_opt)
+S_opt_group = Salmonella_data(S_opt(1,:),1);
+
+D = sqDistances_HCP{1};
+[D_sorted,index_global(:,1)] = min(D(S_opt,1:end),[],1);
+index_global(:,2) = Salmonella_data(1:l,1);
+
+for i=1:K
+    idx = find(index_global(:,2) == i);
+    acc(i,1) = mean(index_global(idx,1) == index_global(idx,2));
 end
---
+++ Codes/Salmonella_old.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 19:40:42 2017
+
+@author: Mehraveh
+"""
+
+
+#%reset
+
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+from sklearn.metrics import precision_recall_fscore_support
+import scipy
+import imp
+import os
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier  #GBM algorithm
+from sklearn import cross_validation, metrics   #Additional scklearn functions
+from sklearn.grid_search import GridSearchCV   #Perforing grid search
+from sklearn.cross_validation import cross_val_score, train_test_split
+from sklearn.ensemble import GradientBoostingClassifier  #GBM algorithm
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from sklearn.linear_model import OrthogonalMatchingPursuit
+from sklearn.linear_model import OrthogonalMatchingPursuitCV
+from sklearn.datasets import make_sparse_coded_signal
+from sklearn.metrics import classification_report
+import plotly.plotly as py
+from sklearn.model_selection import KFold
+from plotly.tools import FigureFactory as FF
+from sklearn.manifold import TSNE
+from sklearn import manifold
+from sklearn.metrics.pairwise import euclidean_distances
+import scipy as sc
+import pylab
+import scipy
+import imp
+import os
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier  #GBM algorithm
+from sklearn import cross_validation, metrics   #Additional scklearn functions
+from sklearn.grid_search import GridSearchCV   #Perforing grid search
+from sklearn.cross_validation import cross_val_score, train_test_split
+from sklearn.ensemble import GradientBoostingClassifier  #GBM algorithm
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from sklearn.linear_model import OrthogonalMatchingPursuit
+from sklearn.linear_model import OrthogonalMatchingPursuitCV
+from sklearn.datasets import make_sparse_coded_signal
+from sklearn.metrics import classification_report
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor  #GBM algorithm
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.cross_validation import LeaveOneOut, cross_val_predict
+import multiprocessing
+from sklearn.metrics import confusion_matrix
+from itertools import combinations
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+from scipy.spatial.distance import pdist, squareform
+
+label = pd.read_csv('/Users/Mehraveh/Downloads/Species101017_labels_new.csv', sep=',', delimiter=None,header=None)
+label = label.drop(0, 1)
+label = label.iloc[0]
+
+data = pd.read_csv('/Users/Mehraveh/Downloads/Species101017_clean_new.csv', sep=',', delimiter=None, header=None)
+data = data.fillna(0)
+
+
+
+### Analaysis with all 9 classes
+data=data.drop(data.index[data[0]==0]) # The first three rows in data are removed as they are meant to be unknown for the model
+data=data.drop(data.index[data[0]==8]) # The first three rows in data are removed as they are meant to be unknown for the model
+data=data.drop(data.index[data[0]==9]) # The first three rows in data are removed as they are meant to be unknown for the model
+X=data
+X=X.drop(0, 1) # The first column in data is the class names, which is removed from X. This is y.
+X=np.asmatrix(X)
+
+y=data[0] # first column of data
+y = y.drop(y.index[data[0]==0])
+y=y.ravel()
+
+#for i in range(174):
+#    for j in range(65):
+#        if (type(X[i,j]) != float):
+#            print(i,j)
+            
+
+#X_bin = 1*(X>0)
+n_samples, n_features = X.shape
+n_digits=2
+
+
+
+### Leave-one-samepl-out cross-validation model
+y_pred = np.zeros(n_samples)
+y_pred_bin = np.zeros(n_samples)
+imp = np.zeros(n_features)
+class_probs = np.zeros([n_samples,np.unique(y).size]) # the probability of assigning each left out sample to each of the classes
+
+
+kf = KFold(n_splits=10)
+kf.get_n_splits(X)
+print(kf)  
+
+    
+#loo = LeaveOneOut(n_samples)
+loo = kf.split(X)
+for train_index, test_index in loo:
+    
+    print(test_index)
+    clf = GradientBoostingClassifier(n_estimators=100,learning_rate=0.05) 
+    clf.fit(X[train_index,:],y[train_index])
+    imp = imp + clf.feature_importances_
+    y_pred[test_index] = clf.predict(X[test_index,:])    
+    class_probs[test_index,:] = clf.predict_proba(X[test_index,:])
+
+
+#    clf = GradientBoostingClassifier(n_estimators=100,learning_rate=0.05) 
+#    clf.fit(X_bin[train_index,:],y[train_index])
+#    y_pred_bin[test_index] = clf.predict(X_bin[test_index,:])    
+
+    
+my_score = np.mean(y_pred==y)
+#my_score_bin = np.mean(y_pred_bin==y)
+print(my_score)
+#print(my_score_bin)
+prob_loo = class_probs
+plt.imshow(prob_loo, cmap=plt.cm.coolwarm, interpolation='none', extent=[0,150,0,np.size(prob_loo,0)])
+plt.grid(True)
+plt.xticks([])
+plt.yticks([])
+ax = plt.gca();
+ax.grid(color='w', linestyle='-', linewidth=0)
+plt.colorbar()
+plt.savefig('/Users/Mehraveh/Desktop/class_probs_leave_one_out.png', dpi=120)
+
+
+sorted_ptms=sorted(range(len(imp)), key=lambda k: imp[k], reverse=True)
+sorted_ptms1 = [x+1 for x in sorted_ptms]
+final = pd.DataFrame(sorted_ptms,label[sorted_ptms1])
+plt.figure(1, figsize=(6, 6))
+plt.figure(figsize=(12, 8))
+xx = range(n_features)
+plt.bar(xx, imp[sorted_ptms], 1, color="blue")
+plt.xticks(xx, label[sorted_ptms1], rotation='vertical')
+plt.savefig('/Users/Mehraveh/Desktop/impFeatures.png', dpi=300)
+
+
+
+
+predicted = y_pred
+y_test = y
+
+precision, recall, fscore, support = precision_recall_fscore_support(y_test, predicted)
+
+
+
+
+### What are the three unknown classes?
+X_test=data.ix[data[0]==0,:]
+X_test=X_test.drop(0, 1)
+X_test=np.asmatrix(X_test)
+y_test=data[0]
+y_test = y_test.ix[data[0]==0]
+y=y.ravel()
+X_train = X
+y_train = y
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+
+class_probs_unknowns = clf.predict_proba(X_test)
+prob_loo_unknowns = class_probs_unknowns
+scipy.io.savemat('/Users/Mehraveh/Documents/MATLAB/Reza/probability_all_9.mat', mdict={'prob_loo': prob_loo, 'correctY': y , 'prob_loo_unknowns': prob_loo_unknowns})
+
+
+### Analaysis with 8 classes after removing contaminated milk
+X=data.drop(data.index[np.where(np.logical_or(data[0]==8,data[0]==0))])
+X=X.drop(0, 1)
+X=np.asmatrix(X)
+
+y=data[0]
+y = y.drop(y.index[np.where(np.logical_or(data[0]==8,data[0]==0))])
+y=y.ravel()
+
+#X_bin = 1*(X>0)
+
+n_samples, n_features = X.shape
+n_digits=2
+
+### Leave-one-samepl-out cross-validation model
+y_pred = np.zeros(n_samples)
+y_pred_bin = np.zeros(n_samples)
+class_probs = np.zeros([n_samples,np.unique(y).size])
+loo = LeaveOneOut(n_samples)
+for train_index, test_index in loo:
+    print(test_index)
+    clf = GradientBoostingClassifier(n_estimators=100,learning_rate=0.05) 
+    clf.fit(X[train_index,:],y[train_index])
+    y_pred[test_index] = clf.predict(X[test_index,:])    
+    class_probs[test_index,:] = clf.predict_proba(X[test_index,:])
+
+#    clf = GradientBoostingClassifier(n_estimators=100,learning_rate=0.05) 
+#    clf.fit(X_bin[train_index,:],y[train_index])
+#    y_pred_bin[test_index] = clf.predict(X_bin[test_index,:])    
+
+my_score = np.mean(y_pred==y)
+#my_score_bin = np.mean(y_pred_bin==y)
+print(my_score)
+#print(my_score_bin)
+prob_loo = class_probs
+plt.imshow(prob_loo, cmap=plt.cm.coolwarm, interpolation='none', extent=[0,150,0,np.size(prob_loo,0)])
+plt.grid(True)
+plt.xticks([])
+plt.yticks([])
+ax = plt.gca();
+ax.grid(color='w', linestyle='-', linewidth=0)
+plt.colorbar()
+plt.savefig('/Users/Mehraveh/Desktop/class_probs_leave_one_out.png', dpi=120)
+
+
+
+### What is contaminated milk assigned to?
+X_test=data.ix[data[0]==8,:]
+X_test=X_test.drop(0, 1)
+X_test=np.asmatrix(X_test)
+y_test=data[0]
+y_test = y_test.ix[data[0]==8]
+y=y.ravel()
+X_train = X
+y_train = y
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+
+class_probs_contaminated = clf.predict_proba(X_test)
+prob_loo_contaminated = class_probs_contaminated
+
+
+### What are the three unknown classes?
+
+X_test=data.ix[data[0]==0,:]
+X_test=X_test.drop(0, 1)
+X_test=np.asmatrix(X_test)
+y_test=data[0]
+y_test = y_test.ix[data[0]==0]
+y_test=y_test.ravel()
+X_train = X
+y_train = y
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+class_probs_unknowns = clf.predict_proba(X_test)
+prob_loo_unknowns = class_probs_unknowns
+
+scipy.io.savemat('/Users/Mehraveh/Documents/MATLAB/Reza/probability_8.mat', mdict={'prob_loo': prob_loo, 'correctY': y , 'prob_loo_contaminated': prob_loo_contaminated, 'prob_loo_unknowns': prob_loo_unknowns})
+
